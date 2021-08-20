4617342ceb2bce34fb2aedd252a64edf1ca62f87
Shubham Thapa <shubhamthapa532@gmail.com>
2021-08-20 21:57:07 +0530

    Website design changed

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,7 @@ const formData = document.querySelector('form');
 const search   = document.querySelector('input');
 const _msg_1   = document.querySelector('#messgae-1')
 const _msg_2   = document.querySelector('#message-2')
+const _msg_3   = document.querySelector('#message-3')
 
 
 formData.addEventListener('submit', (e) => {
@@ -17,6 +18,7 @@ formData.addEventListener('submit', (e) => {
     const val = search.value;
     _msg_1.textContent = 'Loading...'
     _msg_2.textContent = ''
+    _msg_3.textContent = ''
 
     val.length === 0? _msg_1.textContent = 'Search filed must be filled!' : ''
 
@@ -28,9 +30,9 @@ formData.addEventListener('submit', (e) => {
             } else {
                 /* _msg_1.textContent = data.region
                 _msg_2.textContent = data.weather_descriptions */
-                
-                _msg_1.textContent = 'Location : ' + data.place + ', ' + data.region + ', ' + data.country
-                _msg_2.textContent = 'Weather description: Today weather is ' + data.weather_descriptions + ' and temperature is ' + data.temperature
+                _msg_1.textContent = ''
+                _msg_2.textContent = 'Location : ' + data.place + ', ' + data.region + ', ' + data.country
+                _msg_3.textContent = 'Weather description: Today weather is ' + data.weather_descriptions + ' and temperature is ' + data.temperature
             }
         })
     })
